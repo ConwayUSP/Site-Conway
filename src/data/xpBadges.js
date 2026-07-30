4e3186b179dd51d1f50e4d8301eba4632313710c
Guilherme Aika Alves Varella <guilherme.avarella@gmail.com>
2026-07-30 16:17:17 -0300

    feat: xp badges data

---
+++ src/data/xpBadges.js
@@ -0,0 +1,55 @@
+import lvl1 from '../assets/xpBadges/1.png';
+import lvl2 from '../assets/xpBadges/2.png';
+import lvl3 from '../assets/xpBadges/3.png';
+import lvl4 from '../assets/xpBadges/4.png';
+import lvl5 from '../assets/xpBadges/5.png';
+import lvl6 from '../assets/xpBadges/6.png';
+import lvl7 from '../assets/xpBadges/7.png';
+import lvl8 from '../assets/xpBadges/8.png';
+import lvl9 from '../assets/xpBadges/9.png';
+import lvl10 from '../assets/xpBadges/10.png';
+
+const xpBadges = {
+    1: {
+        level: 1,
+        requiredXP: 100,
+        badge: lvl1,
+    },
+    2: {
+        level: 2,
+        requiredXP: 125,
+        badge: lvl2,
+    },
+    3: {
+        level: 3,
+        requiredXP: 150,
+        badge: lvl3,
+    },
+    4: {
+        level: 4,
+        requiredXP: 175,
+        badge: lvl4,
+    },
+    5: {
+        level: 5,
+        requiredXP: 200,
+        badge: lvl5,
+    },
+    6: {
+        level: 6,
+        requiredXP: 225,
+        badge: lvl6,
+    },
+    7: {
+        level: 7,
+        requiredXP: 250,
+        badge: lvl7,
+    },
+    8: {
+        level: "MAX",
+        requiredXP: 275,
+        badge: lvl8,
+    }
+}
+
+export default xpBadges;
